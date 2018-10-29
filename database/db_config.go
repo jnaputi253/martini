e6a7173f903d112ab3318a68c95079ac36d4844a
Juan Naputi <jnaputi@Juans-iMac.local>
2018-10-28 21:51:34 -0500

    Added godocs to public items

--- database/db_config.go
+++ database/db_config.go
@@ -7,12 +7,19 @@ import (
 
 const defaultConfigPath = "config/dbConfig.json"
 
+// DbConfig contains the information used to connect to a database.
 type DbConfig struct {
-	DbVendor string `json:"dbVendor"`
-	Username string `json:"username"`
-	Password string `json:"password"`
+	DbVendor *string `json:"dbVendor"`
+	Username *string `json:"username"`
+	Password *string `json:"password"`
 }
 
+// InitWithDefaultConfigs creates an instance of the DbConfig struct by reading the database vendor
+// and connection information from a configuration file.
+//
+// By default, the name of the file is "dbConfig.json" and it is located in the "config/" directory in the root of this project.
+// If the configuration file has a different name, or is in another directory, then use the InitWithConfigPath function
+// to specify the path to the file.
 func InitWithDefaultConfigs() (*DbConfig, error) {
 	decoder, err := getDecoder(defaultConfigPath)
 	if err != nil {
@@ -22,6 +29,8 @@ func InitWithDefaultConfigs() (*DbConfig, error) {
 	return createDbConfig(decoder)
 }
 
+// InitWithConfigPath creates an instance of the DbConfig struct by reading the databse vendor
+// and connection information from the file specified in the path parameter.
 func InitWithConfigPath(path string) (*DbConfig, error) {
 	decoder, err := getDecoder(path)
 	if err != nil {
